bb0bcea5cdfd494d62b4f7b873facd190e81cefa
forhadmia1 <forhad8613@gmail.com>
2022-08-19 21:23:09 +0600

    api complete with send data

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const express = require('express');
+import express from 'express';
+import fetch from 'node-fetch';
 const app = express()
 const port = 5000;
+
+//middleware
+app.use(express.json())
+
+
+app.get('/users', async (req, res) => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    res.send(data)
+})
 
 app.get('/', (req, res) => {
     res.send('Hexadecimal server is ready')
